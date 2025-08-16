c5bde432ba2fa9996e96aaf58b11d89b76e54e0a
BrantKijin <upper0house@gmail.com>
2025-08-16 15:04:03 +0900

    Integrate JWT-based authentication in `user-service`: add custom `AuthenticationFilter`, implement `UserDetailsService` in `UserService`, and enhance `WebSecurity` configuration with authentication and IP-based access control.

--- user-service/src/main/java/sample/userservice/service/UserService.java
+++ user-service/src/main/java/sample/userservice/service/UserService.java
@@ -1,10 +1,14 @@
 package sample.userservice.service;
 
+import org.springframework.security.core.userdetails.UserDetailsService;
+
 import sample.userservice.dto.UserDto;
 import sample.userservice.jpa.UserEntity;
 
-public interface UserService {
+public interface UserService extends UserDetailsService {
 	UserDto createUser(UserDto userDto);
 	UserDto getUserByUserId(String userId);
 	Iterable<UserEntity> getUserByAll();
+
+	UserDto getUserDetailsByEmail(String userName);
 }
